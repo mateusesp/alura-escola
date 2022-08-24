4904b10c7039fbb22dc2b0e6fe639f231794203f
Mateus Esposito Ferreira <mateus.ferreira@totvs.com.br>
2022-08-24 17:31:55 -0300

    criando classe Aluno e VOs de CPF e email

---
+++ src/main/java/escola/Cpf.java
@@ -0,0 +1,12 @@
+package escola;
+
+public class Cpf {
+    private String numero;
+
+    public Cpf(String numero) {
+        if (numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF invalido!");
+        }
+    }
+}
+
