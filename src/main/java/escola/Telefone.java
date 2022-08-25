56dbe0035753f1d096eff1d706136c223cb00561
Mateus Esposito Ferreira <mateus.ferreira@totvs.com.br>
2022-08-25 10:55:46 -0300

    Criando VOs e factory method

---
+++ src/main/java/escola/Telefone.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Telefone {
+
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+}
