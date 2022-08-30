2ec2b7361e5bcf6bedf4be538808583b39db9146
Mateus Esposito Ferreira <mateus.ferreira@totvs.com.br>
2022-08-30 11:10:18 -0300

    implementando camada de infra do aluno

---
+++ src/main/java/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -0,0 +1,54 @@
+package escola.infra.aluno;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import escola.dominio.aluno.Aluno;
+import escola.dominio.aluno.Cpf;
+import escola.dominio.aluno.RepositorioDeAlunos;
+import escola.dominio.aluno.Telefone;
+
+public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
+
+    private final Connection connection;
+
+    public RepositorioDeAlunosComJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void matricular(Aluno aluno) {
+        try {
+            String sql = "INSERT INTO ALUNO VALUES(?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, aluno.getCpf());
+            ps.setString(2, aluno.getNome());
+            ps.setString(3, aluno.getEmail());
+            ps.execute();
+
+            sql = "INSERT INTO TELEFONE VALUES(?, ?)";
+            ps = connection.prepareStatement(sql);
+            for (Telefone telefone : aluno.getTelefones()) {
+                ps.setString(1, telefone.getDdd());
+                ps.setString(2, telefone.getNumero());
+                ps.execute();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Aluno buscarPorCpf(Cpf cpf) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> listarTodosAlunosMatriculados() {
+        return null;
+    }
+}
