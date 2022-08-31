e3a51acb86fff8253572e927e4da0a2e36623679
Mateus Esposito Ferreira <mateus.ferreira@totvs.com.br>
2022-08-31 16:35:04 -0300

    separando contextos da aplicacao

---
+++ src/test/java/br/com/alura/escola/academico/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -0,0 +1,34 @@
+package br.com.alura.escola.academico.aplicacao.aluno.matricular;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.com.alura.escola.academico.dominio.PublicadorDeEventos;
+import br.com.alura.escola.academico.dominio.aluno.Aluno;
+import br.com.alura.escola.academico.dominio.aluno.CPF;
+import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoDeMatriculado;
+import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import org.junit.jupiter.api.Test;
+
+class MatricularAlunoTest {
+
+    @Test
+    void alunoDeveriaSerPersistido() {
+        // MOCK -> Mockito
+        RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
+        PublicadorDeEventos publicador = new PublicadorDeEventos();
+        
+        publicador.adicionar(new LogDeAlunoDeMatriculado());
+
+        MatricularAluno useCase = new MatricularAluno(repositorio, publicador);
+
+        MatricularAlunoDto dados = new MatricularAlunoDto("Fulano", "123.456.789-00", "fulano@email.com");
+        useCase.executa(dados);
+
+        Aluno encontrado = repositorio.buscarPorCPF(new CPF("123.456.789-00"));
+
+        assertEquals("Fulano", encontrado.getNome());
+        assertEquals("123.456.789-00", encontrado.getCpf());
+        assertEquals("fulano@email.com", encontrado.getEmail());
+    }
+
+}
---
+++ src/test/java/br/com/alura/escola/gamificacao/dominio/selo/SeloTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.escola.gamificacao.dominio.selo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeloTest {
+
+    @Test
+    void test() {
+
+    }
+}
