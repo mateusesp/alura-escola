3d744459544a25a81488ac4174bef6613594c3d6
Mateus Esposito Ferreira <mateus.ferreira@totvs.com.br>
2022-08-31 16:59:54 -0300

    finalizando implementacoes do contexto compartilhado e aplicando refatoracoes

--- src/test/java/br/com/alura/escola/gamificacao/aplicacao/GeraSeloAlunoNovatoTest.java
+++ src/test/java/br/com/alura/escola/gamificacao/aplicacao/GeraSeloAlunoNovatoTest.java
@@ -1,13 +1,14 @@
-package br.com.alura.escola.academico.selo;
+package br.com.alura.escola.gamificacao.aplicacao;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SeloTest {
+class GeraSeloAlunoNovatoTest {
 
     @Test
     void test() {
 
     }
+
 }
